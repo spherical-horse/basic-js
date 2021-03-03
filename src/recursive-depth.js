ce656080596f34e0544ddd937ac67d370c0d56d0
Artem <tema.shevchuk@gmail.com>
2021-03-04 01:32:58 +0300

    feat: add recursive-depth solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,18 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-    calculateDepth(arr, depth = 1) {
-        // console.log(arr);
+    calculateDepth(arr, depth = 1, counters = []) {
+        counters.push(depth);
+        // console.log(counters);
         arr.forEach(element => {
             if (Array.isArray(element)) {
-                this.calculateDepth(element, depth + 1);
-            } else {
-                return depth;
+                this.calculateDepth(element, depth + 1, counters);
             }
         });
-        // return depth;
+        return Math.max.apply(null, counters);
     }
 };
 
 const test = new module.exports();
-console.log(test.calculateDepth([1, 2, 3, [4, 5]]));
+// console.log(test.calculateDepth([1, 2, 3, [4, 5]]));
