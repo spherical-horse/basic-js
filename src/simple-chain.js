86cb144ee95761254f7ac37b0cd9f1b5597deafc
Artem <tema.shevchuk@gmail.com>
2021-03-09 01:20:15 +0300

    feat: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    links: [],
+    getLength() {
+        return links.length;
+    },
+    addLink(value) {
+        this.links.push('' + value);
+        return this;
+    },
+    removeLink(position) {
+        if (typeof(position) !== 'number') {
+            throw new Error();
+        }
+        if (position - 1 < 0 && position - 1 >= this.links.length) {
+            throw new Error();
+        }
+        this.links.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        this.links.reverse();
+        return this;
+    },
+    finishChain() {
+        const links = this.links;
+        this.links = [];
+        return `( ${links.join(' )~~( ')} )`;
+    }
 };
 
 module.exports = chainMaker;
+
+// console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(1).reverseChain().finishChain());
+// console.log(chainMaker.addLink(function() {}).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain());
+// console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(0));
+// console.log(chainMaker.addLink(function() {}).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain());
+// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
+// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
