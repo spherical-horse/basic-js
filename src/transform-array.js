69d1988e09df5e6e9994e472cb884acbf54217ee
Artem <tema.shevchuk@gmail.com>
2021-03-24 18:30:47 +0300

    fix: issue with advanced sequence interactions

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,30 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-    const result = [];
-    let discardNext = false;
-    arr.forEach((element, index) => {
-        if (element === '--discard-prev') {
-            result.pop();
-        } else if (element === '--double-prev') {
-            if (index > 0) {
-                result.push(arr[index - 1]);
-            }
-        } else if (element === '--double-next') {
-            if (index + 1 > arr.length) {
-                result.push(arr[index + 1])
-            }
-        } else if (element === '--discard-next') {
-            discardNext = true;
-        } else {
-            if (!discardNext) {
-                result.push(element);
-            } else {
-                discardNext = false;
-            }
-        }
-    });
-    return result;
+  const result = [];
+  let discardNext = false;
+  arr.forEach((element, index) => {
+    console.log(element);
+    if (element === '--discard-prev') {
+      result[index - 1] = '';
+    } else if (element === '--double-prev') {
+      if (index > 0) {
+        result.push(result[index - 1]);
+      }
+    } else if (element === '--double-next') {
+      if (index + 1 < arr.length) {
+        result.push(arr[index + 1])
+        // console.log(arr)
+      } 
+    } else if (element === '--discard-next') {
+      discardNext = true;
+    } else {
+      if (!discardNext) {
+        result.push(element);
+      } else {
+        result.push('');
+        discardNext = false;
+      }
+    }
+  });
+  const res = result.filter((x) => x !== '' && typeof x !== 'undefined');
+  return res;
 };
 
-// console.log(module.exports([1, 2, 3, '--discard-next', 4, 5]));
+console.log(module.exports([1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5]));
